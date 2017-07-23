40ab22b795e6d8841d04489d12e5e0cddf8ac694
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2017-07-23 18:51:29 +0200

    Update to latest clojure.tools.logging

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                 [org.clojure/tools.logging "0.3.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]
+                 [org.clojure/tools.logging "0.4.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                    :source-paths ["examples"]}})
