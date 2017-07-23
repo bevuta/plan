fbdf626c5e091f686d5c455a78171d2c4a59c0f9
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2017-07-23 18:59:36 +0200

    Refactor interceptors implementation

--- src/bevuta/interceptors.clj
+++ src/bevuta/interceptors.clj
@@ -14,38 +14,31 @@
       (dissoc ::stack)))
 
 (defn execute-1 [ctx stage]
-  (loop [stage' (if (::error ctx)
-                  :error
-                  stage)
-         ctx (-> ctx
+  (loop [ctx (-> ctx
                  (update ::queue seq)
                  (assoc ::stack nil))]
     (if-let [[interceptor] (::queue ctx)]
-      (let [interceptor-fn (get interceptor stage')
+      (let [interceptor-key (if (::error ctx)
+                              :error
+                              stage)
+            interceptor-fn (get interceptor interceptor-key)
             ctx' (-> ctx
                      (update ::stack conj interceptor)
                      (update ::queue next)
                      (cond->> interceptor-fn
                        (call-interceptor-fn interceptor-fn)))
-            ctx' (if (and (::error ctx') (= :error stage') interceptor-fn)
+            ctx' (if (and (::error ctx) (::error ctx') interceptor-fn)
                    (update ctx' ::suppressed-errors conj (::error ctx))
                    ctx')]
-        (cond (::error ctx')
-              (if (= :enter stage')
-                (terminate ctx')
-                (recur :error ctx'))
-
-              (and (= :error stage') (= :enter stage))
-              (terminate ctx')
-
-              :else
-              (recur stage ctx')))
-      (if (= stage' :error)
-        (throw (::error ctx))
+        (if (and (::error ctx') (= :enter stage))
+          (terminate ctx')
+          (recur ctx')))
+      (if-let [error (::error ctx)]
+        (throw error)
         (terminate ctx)))))
 
 (defn execute [ctx]
   (-> ctx
       (execute-1 :enter)
       (execute-1 :leave)
-      (dissoc ::queue ::stack)))
+      (dissoc ::queue ::stack ::error)))
