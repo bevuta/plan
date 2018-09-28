8abe5409cfaf0ba660bee909d5bd06bd785a18c6
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2018-09-28 09:52:07 +0200

    Prepare project.clj for public release

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject bevuta/plan "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject bevuta/plan "1-SNAPSHOT"
+  :description "A clojure.spec flavored adaption of Prismatic^WPlumatic Graph"
+  :url "https://github.com/bevuta/plan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.logging "0.4.0"]]
+                 [org.clojure/tools.logging "0.4.1"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
