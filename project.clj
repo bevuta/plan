dfb90c8a545f43363d7d1a8f247448985343689b
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2020-05-16 10:52:21 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/bevuta/plan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.logging "0.4.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.logging "1.1.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.0.0"]]}})
